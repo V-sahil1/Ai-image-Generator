eedd7335cb0b8eae5315eca124ba385fd59f1d06
V-sahil1 <sahilvardekar1@gmail.com>
2025-03-08 23:33:40 +0530

    margin changes

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -4,18 +4,18 @@ function Footer() {
   return (
     <div className='pt-16 pb-16 bg-black' >
         <div className='w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 items-start pb-8 border-b-2 border-gray-900 space-y-6 sm:space-y-0 '>
-            <div className='cols-span-2  w-[150%]'>
-                <p className='text-2xl sm:text-3xl md:text-4xl text-white  font-bold'>
+            <div className='cols-span-2  '>
+                <p className='text-2xl w-[80%] sm:text-3xl md:text-4xl text-white  font-bold'>
                     We Develop & create digital future
                 </p>
             </div>
-            <div className='ml-[235px] w-[60%]'>
+            <div className=''>
                 <h1 className='text-xl font-bold text-white  '>Address</h1>
                 <p className='text-white opacity-60 mt-4'>Surat</p>
                 <p className='text-white opacity-60 '>Gujrat India</p>
                 
             </div>
-            <div className='ml-[330px]'>
+            <div className=''>
                 <h1 className='text-xl font-bold text-white '>Contact</h1>
                 <p className='text-white opacity-60 mt-4'>Email</p>
                 <p className='text-white sm:text-xl text-lg font-bold opacity-60 '>sahilvardekar89@gmail.com</p>
